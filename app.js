c79b047e8f5ebf290ac431343b2189e45cb74b96
Juansu01 <thejuank0@hotmail.com>
2021-10-22 16:14:21 -0500

    Using api validator to handle requests

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express()
 const { sequelize } = require('./models');
+const path = require('path');
+const OpenApiValidator = require('express-openapi-validator');
 
 app.use(express.json())
+
+const spec = path.join(__dirname, 'api.yaml');
+app.use('/spec', express.static(spec));
+/** */
+app.use(
+    OpenApiValidator.middleware({
+      apiSpec: './api.yaml',
+      validateRequests: true
+    }),
+  );
 
 // holbies endpoints
 app.use('/', require('./routes/holbies'))
@@ -21,6 +33,15 @@ app.use('/', require('./routes/recruiters'))
 
 // holbieList endpoint
 app.use('/', require('./routes/holbie_list'))
+
+app.use((err, req, res, next) => {
+    // 7. Customize errors
+    console.error(err); // dump error to console for debug
+    res.status(err.status || 500).json({
+      message: err.message,
+      errors: err.errors,
+    });
+  });
 
 app.listen({ port: 5000}, async () => {
     console.log('Server up on http://localhost:5000')
