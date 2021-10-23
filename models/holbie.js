9ab75b2aee6173c4fc399f7664840ebca9ad97d4
Andrecast <acastrillonpuerta@gmail.com>
2021-10-23 14:54:28 -0500

    Models, migration and routes for projects

--- models/holbie.js
+++ models/holbie.js
@@ -9,9 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ experience }) {
+    static associate({ experience, projects }) {
       // define association here
       this.hasMany(experience, { foreignKey: 'holbieId', as: 'experiences' })
+      this.hasMany(projects, { foreignKey: 'holbieId', as: 'projects' })
     }
   };
   holbie.init({
