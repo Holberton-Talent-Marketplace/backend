ab7de72970c3654bdde0ed9e056f3ab5e2b33e32
Juansu01 <thejuank0@hotmail.com>
2021-10-28 06:55:29 -0500

    New feature: Api is now able to save profile pictures

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const OpenApiValidator = require('express-openapi-validator');
 const cors = require("cors");
 
+app.use(express.urlencoded({ extended: false }))
 app.use(cors());
 app.use(express.json())
 
--- routes/holbies.js
+++ routes/holbies.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { holbie, experience } = require('../models')
 const validate = require('uuid-validate');
+const fs = require('fs');
 
 const nullable = ["location", "most_amazing_thing", "industries", "github"]
 
 router.get('/holbies', async (req, res) => {
     try {
-        const allHolbies = await holbie.findAll( { include: ['experiences', 'projects', 'capstoneProjects'] })
+        const allHolbies = await holbie.findAll({ include: ['experiences', 'projects', 'capstoneProjects'] })
         return res.json(allHolbies)
 
     } catch (err) {
@@ -40,6 +41,15 @@ router.post('/holbies', async (req, res) => {
         const capId = req.body.capstoneProjectId
         const { gender, name, about_me, location, most_amazing_thing, technologies, industries, linkedin, github } = req.body
         const newHolbie = await holbie.create({ gender, name, about_me, location, most_amazing_thing, technologies, industries, linkedin, github, capstoneProjectId: capId })
+        if (req.files) {
+            console.log(req.files[0].buffer)
+            fs.writeFile(`./profile_pictures/${newHolbie.id}.png`, req.files[0].buffer, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was saved!");
+            });
+        }
         return res.json(newHolbie)
     } catch (err) {
         console.error(err)
@@ -65,7 +75,7 @@ router.put('/holbies/:uuid', async (req, res) => {
             if (holbieById == null) {
                 return res.status(404).json({ message: "holbie not found" })
             } else {
-                const attributes = { gender, name, about_me, location, strengths, skills, most_amazing_thing, technologies, industries, linkedin, github, capstoneProjectId  } = req.body
+                const attributes = { gender, name, about_me, location, strengths, skills, most_amazing_thing, technologies, industries, linkedin, github, capstoneProjectId } = req.body
                 for (let key in attributes) {
                     holbieById[key] = attributes[key]
                 }
