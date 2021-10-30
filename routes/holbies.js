d57d2066a851bdaf3047798e81b81627bf04fac0
Juansu01 <thejuank0@hotmail.com>
2021-10-30 17:25:23 -0500

    Fixing put bug that didn't allow us to update fields

--- routes/holbies.js
+++ routes/holbies.js
@@ -61,22 +61,14 @@ router.post('/holbies', async (req, res) => {
 router.put('/holbies/:uuid', async (req, res) => {
     const uuid = req.params.uuid;
     try {
-        const notNullableAttributes = { gender, name, about_me, technologies, linkedin } = req.body
-        nullable.forEach(element => delete notNullableAttributes[element])
-        console.log(notNullableAttributes)
-        for (let att in notNullableAttributes) {
-            if (notNullableAttributes[att] == null) {
-                return res.status(400).json({ message: `${att} cannot be null` })
-            } else if (notNullableAttributes[att] === "") {
-                return res.status(400).json({ message: `${att} cannot be empty` })
-            }
-        }
+        const attributes = { gender, name, about_me, location, strengths, skills, most_amazing_thing, previous_education, technologies, industries, linkedin, github, capstoneProjectId } = req.body
+        console.log(attributes)
         if (validate(uuid, 4)) {
             const holbieById = await holbie.findByPk(uuid)
             if (holbieById == null) {
                 return res.status(404).json({ message: "holbie not found" })
             } else {
-                const attributes = { gender, name, about_me, location, strengths, skills, most_amazing_thing, technologies, industries, linkedin, github, capstoneProjectId } = req.body
+
                 for (let key in attributes) {
                     holbieById[key] = attributes[key]
                 }
